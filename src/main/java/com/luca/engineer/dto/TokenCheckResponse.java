607c598ade777303574275e3da32b5daf1593480
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-11-30 02:36:30 +0100

    implementato dto necessario per intercettare la risposta del microservizio auth e quindi verificare se il token dell'utente è valido o meno

---
+++ src/main/java/com/luca/engineer/dto/TokenCheckResponse.java
@@ -0,0 +1,14 @@
+package com.luca.engineer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenCheckResponse {
+    private boolean valido;
+}
