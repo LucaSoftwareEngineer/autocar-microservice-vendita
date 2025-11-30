5427609b0f64b91cc5b802c02167bb5f967aeb9d
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-11-30 01:48:52 +0100

    definito entity che mappano mediante ORM le seguenti tabelle del db: auto e vendite

---
+++ src/main/java/com/luca/engineer/models/Auto.java
@@ -0,0 +1,41 @@
+package com.luca.engineer.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Table(name = "auto")
+public class Auto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "auto_id")
+    private Long id;
+
+    @Column(name = "auto_targa", nullable = true)
+    private String targa;
+
+    @Column(name = "auto_marca")
+    private String marca;
+
+    @Column(name = "auto_modello")
+    private String modello;
+
+    @Column(name = "auto_colore")
+    private String colore;
+
+    @Column(name = "auto_numero_ruote")
+    private Integer numeroRuote;
+
+    @Column(name = "auto_cavalli")
+    private Integer cavalli;
+
+    @OneToOne(mappedBy = "auto", fetch = FetchType.EAGER)
+    private Vendita vendita;
+
+}
---
+++ src/main/java/com/luca/engineer/models/Vendita.java
@@ -0,0 +1,34 @@
+package com.luca.engineer.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "vendite")
+public class Vendita {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ven_id")
+    private Long idVendita;
+
+    @Column(name="ven_nominativo_cliente", nullable = false)
+    private String nominativoCliente;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "ven_auto_id", nullable = false)
+    private Auto auto;
+
+    @Column(name = "ven_data_acquisto", nullable = false)
+    private LocalDate dataAcquisto;
+
+}
